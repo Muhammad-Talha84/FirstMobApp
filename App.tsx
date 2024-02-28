073b18f7107af9f7de022c0a1a7d9d99a3c41ebb
Muhammad-Talha84 <talhakakar84@gmail.com>
2024-02-28 22:25:18 +0500

    Created SetUp React-Native

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Talha</Text>
+      <View>
+        <Text>Checking output</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
